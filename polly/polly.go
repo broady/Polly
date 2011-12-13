280df6980a9be6fc30d87063fccefd6ec620ae4b
Chris Broadfoot <chris@chrisbroadfoot.id.au>
2011-12-14 01:54:17 +1100

    voting!

--- polly/polly.go
+++ polly/polly.go
@@ -14,20 +14,27 @@ import (
 )
 
 type Poll struct {
-	Name    string
-	Options []*datastore.Key
-	Owner   string
+	Name       string
+	Owner      string
+	Options    int64
+	TotalVotes int64
+
+	Id int64
 }
 
 type Option struct {
 	Text  string
 	Image string
 	Votes int
+
+	Poll *datastore.Key
+	Id   int64
 }
 
 type Vote struct {
-	Poll    Poll
-	Options []*datastore.Key
+	Owner string
+
+	Option *datastore.Key
 }
 
 var (
@@ -36,14 +43,86 @@ var (
 
 func init() {
 	http.HandleFunc("/poll/", pollHandler)
+	http.HandleFunc("/vote/", voteHandler)
 	http.HandleFunc("/new", newHandler)
 	http.HandleFunc("/add", addHandler)
-	http.HandleFunc("/vote", voteHandler)
 	http.HandleFunc("/", listHandler)
 }
 
 func voteHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) != 4 {
+		http.Error(w, "Must provide a poll id and vote id.", http.StatusBadRequest)
+		return
+	}
+	pollId, err := strconv.Atoi64(parts[2])
+	if err != nil {
+		writeError(c, w, err)
+	}
+	voteId, err := strconv.Atoi64(parts[3])
+	if err != nil {
+		writeError(c, w, err)
+	}
+
+	userId := user.Current(c).Id
+	pollKey := datastore.NewKey(c, "poll", "", pollId, nil)
+	optionKey := datastore.NewKey(c, "option", "", voteId, pollKey)
+
+	voteKey := datastore.NewKey(c, "vote", userId, 0, pollKey)
+
+	vote := new(Vote)
+	option := new(Option)
+	err = datastore.RunInTransaction(c, func(c appengine.Context) os.Error {
+		// Note: this function's argument c shadows the variable c
+		//       from the surrounding function.
+		err := datastore.Get(c, optionKey, option)
+		if err != nil {
+			return err
+		}
+		err = datastore.Get(c, voteKey, vote)
+		if err != nil && err != datastore.ErrNoSuchEntity {
+			return err
+		}
+		if vote.Option != nil {
+			oldOption := new(Option)
+			err := datastore.Get(c, vote.Option, oldOption)
+			if err != nil {
+				return err
+			}
+			oldOption.Votes--
+			_, err = datastore.Put(c, vote.Option, oldOption)
+			if err != nil {
+				return err
+			}
+		} else {
+			poll := new(Poll)
+			err := datastore.Get(c, pollKey, poll)
+			if err != nil {
+				return err
+			}
+			poll.TotalVotes++
+			_, err = datastore.Put(c, pollKey, poll)
+			if err != nil {
+				return err
+			}
+		}
+		option.Votes++
+		vote.Option = optionKey
+		_, err = datastore.Put(c, optionKey, option)
+		if err != nil {
+			return err
+		}
+		_, err = datastore.Put(c, voteKey, vote)
+		return err
+	}, nil)
+	if err != nil {
+		writeError(c, w, err)
+		return
+	}
+	fmt.Fprint(w, "OK")
 }
+
 func addHandler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
@@ -52,14 +131,29 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	if title == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Must enter title. Hit back and try again.")
+		fmt.Fprint(w, "Must enter title.")
 		return
 	}
 
-	options := make([]*datastore.Key, 10)
+	poll := Poll{
+		Name:       title,
+		Owner:      u.Id,
+		TotalVotes: 0,
+	}
+
+	pollKey, err := datastore.Put(c, datastore.NewIncompleteKey(c, "poll", nil), &poll)
+	if err != nil {
+		writeError(c, w, err)
+		return
+	}
 	for i := 1; ; i++ {
 		name := r.FormValue(fmt.Sprintf("title%d", i))
 		if name == "" {
+			if i < 3 {
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprint(w, "Poll must have at least two options.")
+				return
+			}
 			break
 		}
 		img := r.FormValue(fmt.Sprintf("img%d", i))
@@ -68,27 +162,22 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 			Image: img,
 			Votes: 0,
 		}
-		key, err := datastore.Put(c, datastore.NewIncompleteKey(c, "option", nil), &option)
+		_, err := datastore.Put(c, datastore.NewKey(c, "option", "", int64(i), pollKey), &option)
 		if err != nil {
 			writeError(c, w, err)
 			return
 		}
-		options[i-1] = key
+		poll.Options++
 	}
 
-	poll := Poll{
-		Name:    title,
-		Options: options,
-		Owner:   u.Id,
-	}
-
-	key, err := datastore.Put(c, datastore.NewIncompleteKey(c, "poll", nil), &poll)
+	pollKey, err = datastore.Put(c, pollKey, &poll)
 	if err != nil {
 		writeError(c, w, err)
 		return
 	}
-	url := fmt.Sprintf("/poll/%d", key.IntID())
-	http.Redirect(w, r, url, http.StatusFound)
+
+	url := fmt.Sprintf("/poll/%d", pollKey.IntID())
+	fmt.Fprint(w, url)
 }
 
 func newHandler(w http.ResponseWriter, r *http.Request) {
@@ -102,24 +191,13 @@ func pollHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Must provide a poll id.", http.StatusBadRequest)
 		return
 	}
-	id, err := strconv.Atoi64(parts[2])
-	if err != nil {
-		http.Error(w, "Invalid id. Must be an integer.", http.StatusBadRequest)
-		return
-	}
-	poll := new(Poll)
-	key := datastore.NewKey(c, "poll", "", id, nil)
-	err = datastore.Get(c, key, poll)
+	poll, err := fetchPoll(c, parts[2])
 	if err != nil {
 		writeError(c, w, err)
 		return
 	}
 
-	options := make([]interface{}, len(poll.Options))
-	for i := range options {
-		options[i] = new(Option)
-	}
-	err = datastore.GetMulti(c, poll.Options, options)
+	options, err := fetchOptions(c, poll)
 	if err != nil {
 		writeError(c, w, err)
 		return
@@ -128,9 +206,10 @@ func pollHandler(w http.ResponseWriter, r *http.Request) {
 	u := user.Current(c)
 
 	v := map[string]interface{}{
-		"poll": poll,
+		"poll":    poll,
 		"options": options,
-		"super": user.IsAdmin(c) || poll.Owner == u.Id,
+		"super":   user.IsAdmin(c) || poll.Owner == u.Id,
+		"pollid":  parts[2],
 	}
 
 	templates.Execute(w, "poll.html", v)
@@ -140,16 +219,83 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
 
-	v := map[string]string{
-		"foo": "bar",
-		"u":   u.Email,
+	iter := datastore.NewQuery("poll").
+		Filter("Owner =", u.Id).
+		Run(c)
+
+	polls := make([]Poll, 0)
+	for i := 0; ; i++ {
+		var poll Poll
+		key, err := iter.Next(&poll)
+		if err == datastore.Done {
+			break
+		}
+		if err != nil {
+			writeError(c, w, err)
+			return
+		}
+		poll.Id = key.IntID()
+		polls = append(polls, poll)
 	}
 
-	templates.Execute(w, "list.html", v)
+	templates.Execute(w, "list.html", polls)
 }
 
 func writeError(c appengine.Context, w http.ResponseWriter, err os.Error) {
 	c.Errorf(err.String())
 	w.WriteHeader(http.StatusInternalServerError)
 	fmt.Fprint(w, err.String())
+}
+
+func fetchPoll(c appengine.Context, strid string) (*Poll, os.Error) {
+	id, err := strconv.Atoi64(strid)
+	if err != nil {
+		return nil, err
+	}
+	poll := new(Poll)
+	key := datastore.NewKey(c, "poll", "", id, nil)
+	err = datastore.Get(c, key, poll)
+	if err != nil {
+		return nil, err
+	}
+	poll.Id = id
+	return poll, nil
+}
+
+func fetchOptions(c appengine.Context, poll *Poll) ([]*Option, os.Error) {
+	fmt.Printf("%d\n", poll.Id)
+	fmt.Printf("%d\n", poll.Options)
+
+	dst := make([]interface{}, poll.Options)
+	options := make([]*Option, poll.Options)
+	keys := make([]*datastore.Key, poll.Options)
+
+	pollKey := datastore.NewKey(c, "poll", "", poll.Id, nil)
+	fmt.Printf("%v\n", pollKey)
+	fmt.Printf("%v\n", keys)
+	for i := range keys {
+		keys[i] = datastore.NewKey(c, "option", "", int64(i+1), pollKey)
+		dst[i] = new(Option)
+	}
+	fmt.Printf("%v\n", keys)
+	err := datastore.GetMulti(c, keys, dst)
+	fmt.Printf("%s %v\n", err, dst)
+	if err != nil {
+		return nil, err
+	}
+	for i := range dst {
+		opt, ok := dst[i].(*Option)
+		if ok {
+			options[i] = opt
+			opt.Poll = pollKey
+			opt.Id = keys[i].IntID()
+		} else {
+			fmt.Printf("not ok!")
+		}
+	}
+
+	return options, err
+}
+
+func fetchVote(c, poll *Poll, user string) {
 }
